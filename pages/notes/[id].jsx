97c64b238133d9c1593512c4ef0c507b825c66a8
xaster-Kies <samueledward40@gmail.com>
2021-10-08 01:20:12 +0100

    Dynamic query param

--- pages/notes/[id].jsx
+++ pages/notes/[id].jsx
@@ -1,5 +1,16 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 
-const Page = () => <div>Note page</div>
+const Page = () => {
+    const router = useRouter()
+
+    const { id } = router.query
+
+    return (
+        <h1>
+            Note {id}
+        </h1>
+    )
+}
 
 export default Page
